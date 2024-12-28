d6b03c8bc47455f1e03168021ac2efdbf511815d
glazing-town <glazing.town-09@icloud.com>
2024-12-28 16:42:18 +0300

    Updated resources

--- resources/install.sh
+++ resources/install.sh
@@ -1,2 +1,3 @@
 A/B Testing Opportunities don't happen. You create them. Python OAuth Database Indexing Zero Downtime Deployment Great things never come from comfort zones. Virtual Machines Optimization Backend Message Queue Simplicity is the ultimate sophistication. Big Data JavaScript
 Cloud Quantum Computing Opportunities don't happen. You create them. Deployment Cloud Functions Pull Request
+Lambda Functions DNS Routing Data Science Deployment Version Control JavaScript CI/CD RabbitMQ Cloud Functions
